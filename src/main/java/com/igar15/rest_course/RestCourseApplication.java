2253cd373509ae338902a6ac926485745f396dc0
igar15 <igar15@yandex.ru>
2020-10-22 14:57:24 +0300

    initial commit

---
+++ src/main/java/com/igar15/rest_course/RestCourseApplication.java
@@ -0,0 +1,13 @@
+package com.igar15.rest_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCourseApplication.class, args);
+	}
+
+}
